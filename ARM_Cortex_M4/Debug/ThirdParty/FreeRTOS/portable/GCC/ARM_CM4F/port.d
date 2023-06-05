5f07b83077571a9add461640409e0c2dcd758068
Armando Almarales <armandoalmarales@eycon.com>
2023-06-05 20:58:25 -0300

    First Commit - Base Repos for STM32CubeIde: Cortex M3 and M4 based Microcontrollers

---
+++ ARM_Cortex_M4/Debug/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.d
@@ -0,0 +1,34 @@
+ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.o: \
+ ../ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.c \
+ D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h \
+ D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/SEGGER/SEGGER/SEGGER.h \
+ D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/SEGGER/Config/Global.h \
+ D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h \
+ D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h \
+ D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h \
+ D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/FreeRTOS/include/projdefs.h \
+ D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/FreeRTOS/include/portable.h \
+ D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/FreeRTOS/include/task.h \
+ D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/FreeRTOS/include/list.h
+D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
+D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/SEGGER/SEGGER/SEGGER.h:
+D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/SEGGER/Config/Global.h:
+D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h:
+D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h:
+D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
+D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/FreeRTOS/include/projdefs.h:
+D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/FreeRTOS/include/portable.h:
+D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/FreeRTOS/include/task.h:
+D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/FreeRTOS/include/list.h:
---
+++ ARM_Cortex_M4/Debug/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.d
@@ -0,0 +1,6 @@
+ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.o: \
+ ../ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.c \
+ D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
+ D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
+D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
+D:/Armando/OneDrive/Documentos/CURRICULUM/Pruebas\ Entrevistas/FreeRTOS/ARM_Cortex_M4/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
